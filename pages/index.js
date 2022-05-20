644f0132d33c5cd498871e670c0eecfb2b710e67
YooksBoi <81989873+YooksBoi@users.noreply.github.com>
2022-05-20 10:46:58 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,182 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <!DOCTYPE html>
+<html>
+    <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9341297772341434"
+     crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+        <link rel="shortcut icon" href="Pictures/icon.png">
+        <title>Yook's Scripts</title>
+        
+    </head>
+    <body>
+        
+        <section class="header">
+            <nav>
+                <img src="Pictures/icon.png">
+                <div class="links">
+                    <li><a href="index.html" style="color:white">Scripts</a></li>
+                    <li><a href="" style="color:white">Discord</a></li>
+                    <li><a href="about.html" style="color:white">About Me</a></li>
+                </div>
+            </nav>
+            <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+        
+        <div class="Bot-Script">
+            <br>
+            <h1>Bot Script</h1>
+            <br>
+            <a style="color:white" href="Scripts/BSCP">Bot Script Main</a>
+            <br>
+            <br>
+            <a class="bsc" style="color:white" href="Scripts/BCommands">Bot Script Commands</a>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <img class="img"src="Pictures/botimg.png">
     </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="Bot-Script">
+            <br>
+            <h1>FE Dummy [WITH COLLISION]</h1>
+            <br>
+            <a style="color:white" href="Scripts/FeDummyF">FE Dummy [FREE HATS]</a>
+            <br>
+            <br>
+            <a class="bsc" style="color:white" href="Scripts/FeDummyP">FE Dummy [PAID HATS]</a>
+            <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <img class="img"src="Pictures/fedum.png">
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="Bot-Script">
+            <br>
+            <h1>Ultumate TRIA GUI</h1>
+            <br>
+            <a style="color:white" href="Scripts/TRIA">Ultumate TRIA GUI</a>
+            <br>
+            <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <img class="img"src="Pictures/TRIA.png">
+        </div>
+        </section>
+    </body>
+</html>
   )
 }
